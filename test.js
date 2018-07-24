95932e3b5037bb4d9de42eba0d021d000630fd99
CygnusRoboticus <joshuadaleharris@gmail.com>
2018-07-23 20:31:53 -0400

    initial commit

---
+++ test.js
@@ -0,0 +1,43 @@
+import assert from 'assert';
+import { describe, it } from 'mocha';
+import CleverArray from './index';
+
+describe('CleverArray is so smart it can even', () => {
+  describe('#sum', () => {
+    it('all items together', () => {
+      const cleverArray = new CleverArray(1, 2, 3, 4);
+      assert.equal(cleverArray.sum, 10);
+    });
+
+    it('with updates to the array', () => {
+      const cleverArray = new CleverArray([1, 2, 3]);
+      assert.equal(cleverArray.sum, 6);
+      cleverArray.concat([4, 5]);
+      assert.equal(cleverArray.sum, 15);
+    });
+
+    it('an empty array', () => {
+      const cleverArray = new CleverArray();
+      assert.equal(cleverArray.sum, NaN);
+    });
+  });
+
+  describe('#average', () => {
+    it('all array items', () => {
+      const cleverArray = new CleverArray(1, 2, 3, 4);
+      assert.equal(cleverArray.average, 2.5);
+    });
+
+    it('with updates to the array', () => {
+      const cleverArray = new CleverArray([1, 2, 3]);
+      assert.equal(cleverArray.average, 2);
+      cleverArray.concat([4, 5]);
+      assert.equal(cleverArray.average, 3);
+    });
+
+    it('an empty array', () => {
+      const cleverArray = new CleverArray();
+      assert.equal(cleverArray.average, NaN);
+    });
+  });
+});
