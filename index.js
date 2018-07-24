60d70fb1bcaf4d5562acb89a95607abc5a645422
Krishna Glick <krishnaglick@gmail.com>
2018-07-23 21:03:09 -0400

    hey fuk ur dumb thing

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-export default {
-  //implement me
+class SpeshulArray {
+  constructor(...numbers) {
+    this.numbers = numbers;
+  }
+
+  push(...numbers) {
+    this.numbers.push(...numbers);
+  }
+
+  sum() {
+    if(!this.numbers.length) {
+      return NaN;
+    }
+    return this.numbers.reduce((sum, n) => sum + n);
+  }
+
+  average() {
+    if (!this.numbers.length) {
+      return NaN;
+    }
+    return this.sum() / this.numbers.length;
+  }
 }
+
+export default SpeshulArray;
