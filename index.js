b81e7b3fd7556f6492f0e2ba099649d052fbadca
Krishna Glick <krishnaglick@gmail.com>
2018-07-24 21:51:46 -0400

    Removing solution

--- index.js
+++ index.js
@@ -1,25 +1,4 @@
-class SpeshulArray {
-  constructor(...numbers) {
-    this.numbers = numbers;
-  }
 
-  push(...numbers) {
-    this.numbers.push(...numbers);
-  }
-
-  get sum() {
-    if(!this.numbers.length) {
-      return NaN;
-    }
-    return this.numbers.reduce((sum, n) => sum + n);
-  }
-
-  get average() {
-    if (!this.numbers.length) {
-      return NaN;
-    }
-    return this.sum / this.numbers.length;
-  }
-}
-
-export default SpeshulArray;
+export default class {
+  
+};
